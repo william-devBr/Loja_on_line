efc9d3edea5e25ed430e6f6e32d887491ddf7983
WILLIAM BATISTA <53185623+william-devBr@users.noreply.github.com>
2023-07-12 16:23:21 -0300

    Create confirm-cadastro.php
    Página de confirmação de cadastro, informando que foi enviado um codigo de ativação de cadastro por e-mail;

---
+++ core/views/confirm-cadastro.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 text-center my-4">
+             <h1 class="icon-confirm"><i class="fa-solid fa-check"></i></h1>
+             <p>Cadastro efetuado com sucesso</p>
+             <p>Enviamos um link de confirmação por e-mail.<br>
+                Acesse seu e-mail para ativar seu cadastro!</p>
+             <p>Verifique na caixa de <i>SPAM</i> ou <i>Lixo eletrônico</i></p>
+        </div>
+    </div>
+</div>
