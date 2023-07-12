f078877d629fdceeb5eef8bd82def253c0f3563f
WILLIAM BATISTA <53185623+william-devBr@users.noreply.github.com>
2023-07-12 17:00:12 -0300

    Add files via upload
    file config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+/** APP */
+define('APP_NAME','NOME_DA_LOJA');
+define('APP_VERSION','1.0.0');
+
+/** BASE_URL */
+define('BASE_URL', 'localhost/public');
+
+/**MYSQL CONFIG */
+define('MYSQL_SERVER','localhost');
+define('MYSQL_DATABASE','php_store');
+define('MYSQL_USER','root');
+define('MYSQL_PASS','');
+define('MYSQL_CHARSET','utf8');
+
+/** EMAIL*/ 
+define('MAIL_HOST',  'smtp.email.com');
+define('MAIL_CONTA',  'usuario do email');
+define('MAIL_PASS',   'password');
+define('MAIL_PORT',   'porta');
