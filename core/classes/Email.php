b7fd3453d67331ef50dc98c9d54c33574b021f04
WILLIAM BATISTA <53185623+william-devBr@users.noreply.github.com>
2023-07-12 13:55:20 -0300

    Create Email.php
    Classe para envio de e-mails dentro do sistema.
    
    *Necessário configurar a conta no arquivo config.php

---
+++ core/classes/Email.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace core\classes;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+class Email {
+
+    public static function confirmacao_email($usuario){
+
+        //Create an instance; passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_OFF;                       //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = MAIL_HOST;                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = MAIL_CONTA;                     //SMTP username
+            $mail->Password   = MAIL_PASS;                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = MAIL_PORT;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            $mail->setFrom(MAIL_CONTA, APP_NAME);
+            $mail->addAddress('');     //Add a recipient
+          
+            //Attachments
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = APP_NAME. '- Cofirmação de e-mail';
+            $html = ' ';
+            $mail->Body    = 'Confirmação de e-mail <b>in bold!</b>';
+            
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+     }
+}
